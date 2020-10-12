7e5de5953406def03384c6299de135e259e32630
Rumana <rumanaparven16@gmail.com>
2020-10-12 19:57:06 +0530

    [Rumana] Add. Message returned based on mood.

---
+++ MoodAnalyser/MoodAnalyser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyser
+{
+    public class Mood
+    {
+        private string message;
+        public Mood()
+        {
+
+        }
+        public Mood(string message)
+        {
+            this.message = message;
+        }
+        public string AnalayseMood()
+        {
+            if (this.message.Contains("SAD"))
+                return "SAD";
+            else if (this.message.Contains("HAPPY"))
+                return "SAD";
+            else
+                return "HAPPY";
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,37 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoodAnalyser;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string expected = "SAD";
+            string message = "I am SAD";
+            Mood mood = new Mood(message);
+            string actual = mood.AnalayseMood();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string expected = "HAPPY";
+            string message = "I am in any mood";
+            Mood mood = new Mood(message);
+            string actual = mood.AnalayseMood();
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            string expected = "SAD";
+            string message = "I am in HAPPY mood";
+            Mood mood = new Mood(message);
+            string actual = mood.AnalayseMood();
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
